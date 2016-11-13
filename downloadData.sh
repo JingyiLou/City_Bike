ea61a684e2ab67e701b29f0db0ba88aa2706ea5d
Jingyi Lou <jl8166@nyu.edu>
2016-11-13 17:36:54 -0500

    added new datas

---
+++ downloadData.sh
@@ -0,0 +1,9 @@
+for i in $(seq 9); do
+  if ! [ -f "20160$i-citibike-tripdata.csv" ]; then
+    if ! [ -f "20160$i-citibike-tripdata.zip" ]; then
+      wget http://witestlab.poly.edu/bikes/"20160$i-citibike-tripdata.zip"
+    fi
+  unzip "20160$i-citibike-tripdata.zip"
+  fi
+done
+
